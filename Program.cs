0a287b5605b45b2575a72c81ff09466c2cd27d85
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2024-12-23 10:23:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace поиск_последовательности
+{
+    internal class Program
+    {
+        public static int count_max(string path)
+        {
+            try
+            {
+                string text = File.ReadAllText(path);
+
+                string regularka = @"(?:AB|CB)+";
+                MatchCollection matches = Regex.Matches(text, regularka);
+                int maxCount = 0;
+                foreach (Match match in matches)
+                {
+                    maxCount = Math.Max(maxCount, match.Length / 2);
+                }
+                return maxCount;
+
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл не найден.");
+                return -1;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            string mypath = "C:\\Users\\Users\\OneDrive\\Документы\\учеба\\Системное программирование\\ABB BA A B BB.txt";
+            int max = count_max(mypath);
+
+            if (max != -1)
+            {
+                Console.WriteLine($"Максимальное количество подряд идущих пар: {max}");
+            }
+        }
+    }
+}
